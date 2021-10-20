90d144a8076032ac3390b85f4501d4049e8a3ed8
P Kry <piter71-1997@go2.pl>
2021-10-21 01:21:14 +0200

    Authentication config

--- src/main/java/com/kryhowsky/shop/security/JwtAuthenticationFilter.java
+++ src/main/java/com/kryhowsky/shop/security/JwtAuthenticationFilter.java
@@ -1,8 +1,27 @@
 package com.kryhowsky.shop.security;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kryhowsky.shop.model.dto.LoginDto;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.impl.DefaultClaims;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationServiceException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Date;
+import java.util.stream.Collectors;
 
 public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
@@ -14,4 +33,33 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
         setFilterProcessesUrl("/api/login");
     }
 
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        if (!request.getMethod().equals(HttpMethod.POST.name())) {
+            throw new AuthenticationServiceException("Not valid HTTP method: " + request.getMethod());
+        }
+
+        try {
+            LoginDto loginDto = objectMapper.readValue(request.getInputStream(), LoginDto.class);
+            UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(loginDto.getEmail(), loginDto.getPassword());
+            return getAuthenticationManager().authenticate(usernamePasswordAuthenticationToken);
+        } catch (IOException e) {
+            throw new AuthenticationServiceException(e.getMessage());
+        }
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+        Claims claims = new DefaultClaims()
+                .setSubject(authResult.getName())
+                .setExpiration(new Date(System.currentTimeMillis() + 24 * 60 * 60 * 1000));
+        claims.put("authorities", authResult.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining(",")));
+
+        String token = Jwts.builder()
+                .setClaims(claims)
+                .signWith(SignatureAlgorithm.HS512, "akdsbgkasdbixzvzov345hkjefbgmvx")
+                .compact();
+    }
 }
--- src/main/java/com/kryhowsky/shop/security/UserDetailsServiceImpl.java
+++ src/main/java/com/kryhowsky/shop/security/UserDetailsServiceImpl.java
@@ -2,10 +2,14 @@ package com.kryhowsky.shop.security;
 
 import com.kryhowsky.shop.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -15,7 +19,11 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        return null;
+        return userRepository.findByEmail(email)
+                .map(user -> new User(email, user.getPassword(), user.getRoles().stream()
+                        .map(role -> new SimpleGrantedAuthority(role.getName()))
+                        .collect(Collectors.toList())))
+                .orElseThrow(() -> new UsernameNotFoundException(email));
     }
 
 }
