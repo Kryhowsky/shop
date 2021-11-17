ac5f9452f5a9cc91953d1d7ef0daa92b80f1cab6
P Kry <piter71-1997@go2.pl>
2021-11-17 22:19:43 +0100

    XLSGenerator implemented

--- src/main/java/com/kryhowsky/shop/controller/FileController.java
+++ src/main/java/com/kryhowsky/shop/controller/FileController.java
@@ -5,6 +5,9 @@ import com.kryhowsky.shop.flyweight.generic.strategy.file.FileGeneratorStrategy;
 import com.kryhowsky.shop.flyweight.model.FileType;
 import com.kryhowsky.shop.flyweight.standard.GeneratorFactory;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -24,8 +27,13 @@ public class FileController {
     }
 
     @GetMapping("/generic")
-    public void testGenericFlyweight(@RequestParam FileType fileType) {
-        genericFactory.getStrategyByType(fileType).generateFile();
+    public ResponseEntity<byte[]> testGenericFlyweight(@RequestParam FileType fileType) { // use ResponseEntity only when custom headers needed
+        byte[] file = genericFactory.getStrategyByType(fileType).generateFile();
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.set(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_OCTET_STREAM_VALUE);
+        httpHeaders.set(HttpHeaders.CONTENT_LENGTH, String.valueOf(file.length));
+        httpHeaders.set(HttpHeaders.CONTENT_DISPOSITION, "attachment;filename=report." + fileType.name().toLowerCase());
+        return ResponseEntity.ok().headers(httpHeaders).body(file);
     }
 
 }
--- src/main/java/com/kryhowsky/shop/flyweight/generic/strategy/file/impl/XlsFileGenerator.java
+++ src/main/java/com/kryhowsky/shop/flyweight/generic/strategy/file/impl/XlsFileGenerator.java
@@ -2,12 +2,27 @@ package com.kryhowsky.shop.flyweight.generic.strategy.file.impl;
 
 import com.kryhowsky.shop.flyweight.generic.strategy.file.FileGeneratorStrategy;
 import com.kryhowsky.shop.flyweight.model.FileType;
+import com.kryhowsky.shop.model.dao.Product;
+import com.kryhowsky.shop.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.apache.poi.ss.util.CellRangeAddress;
 import org.springframework.stereotype.Component;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.List;
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class XlsFileGenerator implements FileGeneratorStrategy {
+
+    private final ProductRepository productRepository;
 
     @Override
     public FileType getType() {
@@ -17,6 +32,48 @@ public class XlsFileGenerator implements FileGeneratorStrategy {
     @Override
     public byte[] generateFile() {
         log.info("Generating XLS file using Generic Strategy..");
+
+        try {
+            Workbook workbook = WorkbookFactory.create(false);
+            Sheet sheet = workbook.createSheet();
+            Row row = sheet.createRow(0);
+            row.createCell(0).setCellValue("Id");
+            row.createCell(1).setCellValue("Brand");
+            row.createCell(2).setCellValue("Description");
+            row.createCell(3).setCellValue("Name");
+//            row.createCell(4).setCellValue("Price");
+//            row.createCell(5).setCellValue("Quantity");
+//            row.createCell(6).setCellValue("Created By");
+//            row.createCell(7).setCellValue("Created Date");
+//            row.createCell(8).setCellValue("Last Modified By");
+//            row.createCell(9).setCellValue("Last Modified Date");
+
+            List<Product> productList = productRepository.findAll();
+
+            int index = 1;
+            for (Product product : productList) {
+                var productRow = sheet.createRow(index++);
+                productRow.createCell(0).setCellValue(product.getId());
+                productRow.createCell(1).setCellValue(product.getBrand());
+                productRow.createCell(2).setCellValue(product.getDescription());
+                productRow.createCell(3).setCellValue(product.getName());
+//                productRow.createCell(4).setCellValue(product.getPrice());
+//                productRow.createCell(5).setCellValue(product.getQuantity());
+//                productRow.createCell(6).setCellValue(product.getCreatedBy());
+//                productRow.createCell(7).setCellValue(product.getCreatedDate());
+//                productRow.createCell(8).setCellValue(product.getLastModifiedBy());
+//                productRow.createCell(9).setCellValue(product.getLastModifiedDate());
+            }
+
+            sheet.setAutoFilter(new CellRangeAddress(0, index, 0, 3));
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            workbook.write(byteArrayOutputStream);
+            return byteArrayOutputStream.toByteArray();
+
+        } catch (IOException e) {
+            log.error("Cannot generate Excel file!", e);
+        }
+
         return new byte[0];
     }
 }
